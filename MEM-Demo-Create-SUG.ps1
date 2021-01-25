7284488ded51ed64ec22ce8f233ac8ce0fba18f9
Mike Bishop <76496950+BishopCloud@users.noreply.github.com>
2021-01-24 21:22:11 -0600

    Update MEM-Demo-Create-SUG.ps1

--- MEM-Demo-Create-SUG.ps1
+++ MEM-Demo-Create-SUG.ps1
@@ -1,20 +1,12 @@
-<#	
-	.NOTES
-	===========================================================================
+#region connection
+# Press 'F5' to run this script. Running this script will load the ConfigurationManager
+# module for Windows PowerShell and will connect to the site.
+#
+# This script was auto-generated at '1/19/2021 8:38:57 AM'.
 
-	 Created on:   	12/15/2020
-	 Created by:   	Mike Bishop
-	 Organization: 	
-	 Filename:  MEM-Demo-Create-SUG.ps1   	
-	===========================================================================
-	.DESCRIPTION
-    Connects to the specified SiteCode and SMSProvider supplied below.  Then gathers a list of updates based on the selected critera and creates a MEM Software Update Group with those updates.
-		
-
-#>
-
-cls
-#region MEMConnection
+# Uncomment the line below if running in an environment where script signing is 
+# required.
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
 # Site configuration
 $SiteCode = "EM1" # Site code 
@@ -24,6 +16,8 @@ $ProviderMachineName = "FMM31597MEM001.backyard.corp" # SMS Provider machine nam
 $initParams = @{}
 #$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
 #$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Do not change anything below this line
 
 # Import the ConfigurationManager.psd1 module 
 if((Get-Module ConfigurationManager) -eq $null) {
@@ -38,29 +32,76 @@ if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue
 # Set the current location to be the site code.
 Set-Location "$($SiteCode):\" @initParams
 
-#endregion 
+#endregion
 
-#Set-CMQueryResultMaximum -Maximum 5000
 
-$NewSUGName = "2020-DECEMBER-REQUIRED"
-$DatePostedMin = "11/11/2020"
-$DatePostedMax = '12/9/2020'
 
-#$Updates = Get-CMSoftwareUpdate -Fast -DateRevisedMin $DatePostedMin -DateRevisedMax $DatePostedMax | where-object {$_.IsSuperseded -eq $False -and $_.IsExpired -eq $False  -and $_.ISEnabled -eq $True -and $_.IsContentProvisioned -eq $True -and $_.LocalizedDisplayName -notlike "*Windows Malicious Software Removal Tool*" -and $_.LocalizedDisplayName -notlike "*Itanium*" -and $_.LocalizedDisplayName -notlike "*IA64*" -and $_.LocalizedDisplayName -notlike "*2003*" -and $_.LocalizedDisplayName -notlike "*1803*"   -and $_.LocalizedDisplayName -notlike "*1709*"   -and $_.LocalizedDisplayName -notlike "*2000*"  -and $_.LocalizedDisplayName -notlike "*2007*" -and $_.LocalizedDisplayName -notlike "*Skype*" -and $_.LocalizedDisplayName -notlike "*Office*" -and $_.LocalizedDisplayName -notlike "*Visio*" -and $_.LocalizedDisplayName -notlike "*Publisher*" -and $_.LocalizedDisplayName -notlike "*Word*" -and $_.LocalizedDisplayName -notlike "*Project*" -and $_.LocalizedDisplayName -notlike "*Powerpoint*" -and $_.LocalizedDisplayName -notlike "*OneNote*" -and $_.LocalizedDisplayName -notlike "*OneDrive*" -and $_.LocalizedDisplayName -notlike "*Access*" -and $_.LocalizedDisplayName -notlike "*Infopath*" -and $_.LocalizedDisplayName -notlike "*Excel*" -and $_.LocalizedDisplayName -notlike "*Microsoft SQL Server* Service Pack*"}| select BulletinID,CI_ID,ISContentProvisioned,Title,LocalizedDisplayName,IsSuperseded,category
-#$Updates = Get-CMSoftwareUpdate -Fast -DateRevisedMin $DatePostedMin -DateRevisedMax $DatePostedMax | where-object {$_.IsSuperseded -eq $False -and $_.IsExpired -eq $False -and $_.LocalizedDisplayName -notlike "*Windows Malicious Software Removal Tool*" -and $_.LocalizedDisplayName -notlike "*Itanium*" -and $_.LocalizedDisplayName -notlike "*IA64*" -and $_.LocalizedDisplayName -notlike "*2003*" -and $_.LocalizedDisplayName -notlike "*1803*"   -and $_.LocalizedDisplayName -notlike "*1709*"   -and $_.LocalizedDisplayName -notlike "*2000*"  -and $_.LocalizedDisplayName -notlike "*2007*" -and $_.LocalizedDisplayName -notlike "*Skype*" -and $_.LocalizedDisplayName -notlike "*Office*" -and $_.LocalizedDisplayName -notlike "*Visio*" -and $_.LocalizedDisplayName -notlike "*Publisher*" -and $_.LocalizedDisplayName -notlike "*Word*" -and $_.LocalizedDisplayName -notlike "*Project*" -and $_.LocalizedDisplayName -notlike "*Powerpoint*" -and $_.LocalizedDisplayName -notlike "*OneNote*" -and $_.LocalizedDisplayName -notlike "*OneDrive*" -and $_.LocalizedDisplayName -notlike "*Access*" -and $_.LocalizedDisplayName -notlike "*Infopath*" -and $_.LocalizedDisplayName -notlike "*Excel*" -and $_.LocalizedDisplayName -notlike "*Microsoft SQL Server* Service Pack* " -and $_.LocalizedDisplayName -notlike "*Exchange*"}| select BulletinID,CI_ID,ISContentProvisioned,Title,LocalizedDisplayName,IsSuperseded,category
-$Updates = Get-CMSoftwareUpdate -Fast -DateRevisedMin $DatePostedMin -DateRevisedMax $DatePostedMax | where-object {$_.IsSuperseded -eq $False -and $_.IsExpired -eq $False -and $_.LocalizedDisplayName -notlike "*Itanium*" -and $_.LocalizedDisplayName -notlike "*IA64*"}| select BulletinID,CI_ID,ISContentProvisioned,Title,LocalizedDisplayName,IsSuperseded,category
 
-#$Updates = Get-CMSoftwareUpdate -Fast -DateRevisedMin $DatePostedMin -DateRevisedMax $DatePostedMax | where-object {$_.NumMissing -gt 0}
+
+
+
+
+
+
+
+
+
+
+
+
+#region DemoFull
+cls
+$NewSUGName = "2021-JANUARY-NEEDED"
+$DatePostedMin = "1/12/2021"  #When not specifiing the time in the datetime object it defaults to 12:00 AM.
+$DatePostedMax = '1/13/2021'  #When not specifiing the time in the datetime object it defaults to 12:00 AM.
+
+# We are filtering the update list based on the Date/Time range provided above.  Also filtering out superseded and expired updates plus including only updates that are showing to have systems needing them in our environment.
+$Updates = Get-CMSoftwareUpdate -Fast -DateRevisedMin $DatePostedMin -DateRevisedMax $DatePostedMax | where-object {$_.IsSuperseded -eq $False -and $_.IsExpired -eq $False -and $_.NumMissing -gt 0}| select BulletinID,CI_ID,ISContentProvisioned,Title,LocalizedDisplayName,IsSuperseded,NumMissing
+
 
 If($Updates.count -lt 1000)
 {
-    write-host "Found $($Updates.count) that match expected criteria."
-    $UG = New-CMSoftwareUpdateGroup -Name $NewSUGName  -UpdateId $Updates.CI_ID 
+    Write-Host ""
+    $Updates.localizeddisplayname
+    Write-Host ""
+    write-host "Found $($Updates.count) updates that match expected criteria." 
+    $Answer = Read-Host -Prompt "Do you want to create SUG using the updates listed above? ( Y / N)"
+     if($Answer -eq "Y")
+        {
+            $UG = New-CMSoftwareUpdateGroup -Name $NewSUGName  -UpdateId $Updates.CI_ID 
+            Write-Host $("Created SUG: " + $UG.LocalizedDIsplayName + " on " + $UG.DateCreated + " by " + $UG.CreatedBy)
+
+        }
+     else
+        {
+            Write-Host "Exiting without creating SUG"
+        }
 }
 Else
 {
-    write-host "More than one thousand ($($Updates.count)) ,need to further define the critera."
+    write-host "More than one thousand ($($Updates.count)) , need to further refine the critera."
 }
 
 
-$Updates.LocalizedDisplayName
+
+#endregion
+
+
+
+<#region DemoSimple
+
+$NewSUGName = "2021-JANUARY-NEEDED"
+$DatePostedMin = "1/12/2021"
+$DatePostedMax = '1/13/2021'
+
+$Updates = Get-CMSoftwareUpdate -Fast -DateRevisedMin $DatePostedMin -DateRevisedMax $DatePostedMax | where-object {$_.IsSuperseded -eq $False -and $_.IsExpired -eq $False -and $_.NumMissing -gt 0}| select BulletinID,CI_ID,ISContentProvisioned,Title,LocalizedDisplayName,IsSuperseded,NumMissing
+
+$Updates.count
+
+$Updates.localizeddisplayname
+
+New-CMSoftwareUpdateGroup -Name $NewSUGName  -UpdateId $Updates.CI_ID
+
+#endregion
+
+#>
